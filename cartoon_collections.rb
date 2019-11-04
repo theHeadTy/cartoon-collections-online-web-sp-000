ef2bfd0cd6c8b3d5ba2fd37327d6c432e06cf6bf
generic-puppet-7755 <tasmith1990@gmail.com>
2019-11-04 01:46:06 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,42 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+  dwarves.map.with_index do |name, index|
+    puts "#{index += 1}. #{name}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  new = []
+  array.map do |val|
+    val.concat('!').capitalize
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  calls.all? { |call| call.size <= 4 }
+  calls.any? { |call| call.size > 4 }
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
+def find_the_cheese(cheese)
+  new = []
+  types = ['cheddar', 'gouda', 'camembert']
+  cheese.detect { |c| types.include?(c) }
 end
+
+dwarves = %w[Dopey Grumpy Bashful]
+roll_call_dwarves(dwarves)
+
+veggies = %w[carrot cucumber pepper]
+fruits = %w[apple banana orange]
+summon_captain_planet(veggies)
+summon_captain_planet(fruits)
+
+long = ["axe", "earth", "wind", "fire"]
+short = ["wind", "fire", "tree", "axe", "code"]
+long_planeteer_calls(long)
+long_planeteer_calls(short)
+
+cheddar_cheese = %w[banana cheddar sock]
+find_the_cheese(cheddar_cheese)
+
+no_cheese = %w[ham cellphone computer]
+find_the_cheese(no_cheese)
